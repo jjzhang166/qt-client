0cdd23ecf8050061f48c899bd07b8a42157ac803
sunsetsolutions <sunsetsolutions>
2010-08-04 03:29:26 +0000

    Update version for 3.5.3
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/branches/from3_5_1rc@9811 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- guiclient/version.cpp
+++ guiclient/version.cpp
@@ -13,8 +13,8 @@
 QString _Name = "xTuple ERP: %1 Edition";
 
 #ifndef __USEALTVERSION__
-QString _Version   = "3.5.2";
-QString _dbVersion = "3.5.2";
+QString _Version   = "3.5.3";
+QString _dbVersion = "3.5.3";
 #else
 #include "../altVersion.cpp"
 #endif
