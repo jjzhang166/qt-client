00f96ddb7def06b73efb577cd6c1ef9fd5dc6c9a
cryan <cryan>
2010-09-16 20:01:35 +0000

    Backported issue #12014 for a special 3.5.4i release.
    git-svn-id: http://svn.code.sf.net/p/postbooks/code/xtuple/branches/from_35x@10175 ea1a15ef-c238-0410-93cb-fa28e9f07733

--- guiclient/version.cpp
+++ guiclient/version.cpp
@@ -13,7 +13,7 @@
 QString _Name = "xTuple ERP: %1 Edition";
 
 #ifndef __USEALTVERSION__
-QString _Version   = "3.5.4";
+QString _Version   = "3.5.4i";
 QString _dbVersion = "3.5.4";
 #else
 #include "../altVersion.cpp"
