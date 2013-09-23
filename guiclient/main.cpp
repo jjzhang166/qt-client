6e8996b77e6ccd759fd23d04da5ee44e08cf6520
Greg Pazo <gpazo@xtuple.com>
2013-09-23 11:27:56 -0400

    tagging 4.2.0RC release

--- guiclient/main.cpp
+++ guiclient/main.cpp
@@ -479,7 +479,7 @@ int main(int argc, char *argv[])
   metric.exec("SELECT metric_value"
            "  FROM metric"
            " WHERE (metric_name = 'ServerVersion')" );
-  if(!metric.first() || (metric.value("metric_value").toString() != _dbVersion))
+  if(!metric.first() || (metric.value("metric_value").toString() != _Version))
   {
     bool disallowMismatch = false;
     metric.exec("SELECT metric_value FROM metric WHERE(metric_name='DisallowMismatchClientVersion')");
@@ -488,12 +488,39 @@ int main(int argc, char *argv[])
     
     _splash->hide();
 
+    int result;
+         if(disallowMismatch)
+          result = QMessageBox::warning( 0, QObject::tr("Version Mismatch"),
+             QObject::tr("<p>The version of the database you are connecting to is "
+                           "not the version this client was designed to work against. "
+                           "This client was designed to work against the database "
+                           "version %1. The system has been configured to disallow "
+                           "access in this case.<p>Please contact your systems "
+                           "administrator.").arg(_Version),
+                        QMessageBox::Ok | QMessageBox::Escape | QMessageBox::Default );
+           else
+             result = QMessageBox::warning( 0, QObject::tr("Version Mismatch"),
+               QObject::tr("<p>The version of the database you are connecting to is "
+                           "not the version this client was designed to work against. "
+                           "This client was designed to work against the database "
+                           "version %1. If you continue some or all functionality may "
+                           "not work properly or at all. You may also cause other "
+                           "problems on the database.<p>Do you want to continue "
+                           "anyway?").arg(_Version),
+                        QMessageBox::Yes,
+                        QMessageBox::No | QMessageBox::Escape | QMessageBox::Default );
+           if(result != QMessageBox::Yes)
+             return 0;
+
+       //end temporary code
+
+    /*
     checkForUpdates newdlg(0,"", TRUE);
 
     int result = newdlg.exec();
     if (result == QDialog::Rejected)
         return 0;
-
+    */
     _splash->show();
   }
 
